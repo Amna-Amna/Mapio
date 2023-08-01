4a65dbe5a6a77f089b3bf617bafa216a06fb3cdd
Amna-Amna <mailtoamna4@gmail.com>
2023-08-02 02:36:58 +0500

    feat: Implement form submission process, validate input, create workout object based on type, and render map marker.

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ class Workout {
 }
 
 class Running extends Workout {
+  type ='running'
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
@@ -36,6 +37,8 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type ='cycling'
+
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
@@ -52,6 +55,8 @@ class Cycling extends Workout {
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
+
 
   constructor() {
     this._getPosition();
@@ -101,11 +106,40 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...Inputs) => Inputs.every(inp => Number.isFinite(inp))
+    const allPositive = (...Inputs) => Inputs.every(inp => inp > 0)
     e.preventDefault();
 
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    const type = inputType.value
+    const distance = +inputDistance.value
+    const duration = +inputDuration.value
     const { lat, lng } = this.#mapEvent.latlng;
-    L.marker([lat, lng])
+    let workout;
+
+    if(type === 'running'){
+      const cadence = +inputCadence.value
+      if(!validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence)) return alert("Invalid input")
+      workout = new Running([lat, lng], distance, duration, cadence)
+    }
+
+
+    if(type === 'cycling'){
+      const elevation = +inputElevation.value
+      if(!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) return alert("Invalid input")
+      workout = new Cycling([lat, lng], distance, duration, elevation)
+
+    }
+    this.#workouts.push(workout)
+    console.log(workout)
+
+    this.renderWorkoutMarker(workout)
+
+      inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+  }
+  renderWorkoutMarker(workout){
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -113,12 +147,13 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('Running')
+      .setPopupContent(workout.distance + '')
       .openPopup();
   }
 }
+
 
 const app = new App();
