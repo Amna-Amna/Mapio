f774c3304c5d8ea33453a0c0c433eb1da9f2fc03
Amna-Amna <mailtoamna4@gmail.com>
2023-08-02 00:46:43 +0500

    feat: Impliment workout classes with properties like date, ID, coordinates, distance, duration, cadence, pace, and speed.

--- script.js
+++ script.js
@@ -11,6 +11,44 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  constructor(coords, distance, duration) {
+    this.date = new Date(); 
+    this.id = (Date.now() + '').slice(-10); 
+
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed(); 
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+
 class App {
   #map;
   #mapEvent;
