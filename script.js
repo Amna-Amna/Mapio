2044030585264f237c19e99a874d2686d6487387
Amna-Amna <mailtoamna4@gmail.com>
2023-08-01 19:33:54 +0500

    feat: Integrated Leaflet Library for Interactive Map

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      let coords = [latitude, longitude];
+
+      map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        console.log(mapEvent);
+      });
+    },
+    function () {
+      alert("Couldn't get our location");
+    }
+  );
+}
+
